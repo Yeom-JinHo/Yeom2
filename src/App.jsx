316bfb4ee01803bdbafe02edad0d80b209d28f5f
Yeom-JinHo <pwlsghq@naver.com>
2022-03-20 20:59:10 +0900

    feat : Img --> Dots

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./reset.css";
+import Canvas from "./components/Canvas.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
