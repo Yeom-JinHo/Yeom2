3e6423412f9beaf55f015f4a27ce31031834bc36
Yeom-JinHo <pwlsghq@naver.com>
2022-04-06 01:56:25 +0900

    style : 전역 스타일 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "../src/assets/styles/reset.css";
+import "../src/assets/styles/font.css";
+import "../src/assets/styles/_variables.scss";
+import "../src/assets/styles/_utils.scss";
 
 ReactDOM.render(
   <React.StrictMode>
