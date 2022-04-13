bfb3c83537d18f418ccfb83910c11f7cdaf4b829
Yeom-JinHo <pwlsghq@naver.com>
2022-04-13 12:38:37 +0900

    fix : 프로필

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Profile.scss";
 const Profile = () => {
+  const profileData = [
+    "모였을때 더 빛나는 협업에 능한",
+    `'왜?' 라는 질문을 중요하게 생각하는`,
+    "T자형 인재를 지향하는",
+  ];
   return (
     <div className="profile-page">
-      {/* <img src={cloudImg} alt="구름이미지" className="profile__cloud-img" /> */}
-      <div className="profile__cloud-img"></div>
       <header className="profile-header">저는 ___ 개발자 입니다.</header>
       <ul className="profile-ul">
-        <li className="profile-ul__li">1</li>
-        <li className="profile-ul__li">2</li>
-        <li className="profile-ul__li">3</li>
-        <li className="profile-ul__li">4</li>
+        {profileData.map((item, ind) => (
+          <li className="profile-ul__li">
+            {ind + 1}. {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
