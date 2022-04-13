bca75a23a0afce01a0e7f56203ab879bf9d6154d
Yeom-JinHo <pwlsghq@naver.com>
2022-04-13 16:27:48 +0900

    feat : 별들 패럴럭스

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import Skills from "./Pages/Skills";
 import Projects from "./Pages/Projects";
 import SideScroll from "./components/common/SideScroll";
 import bottomImg from "../src/assets/img/bottom.png";
+import starImg from "../src/assets/img/star.png";
+import { useEffect, useRef } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  const starBgRef = useRef();
+  AOS.init();
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      var scroll = window.scrollY;
+      starBgRef.current.style.transform = "translateY(" + -scroll / 3 + "px)";
+    });
+  });
   return (
     <div className="App">
       <Canvas />
       <SideScroll />
       <Profile />
-      <Skills />
+      <Skills data-aos="fade-up" data-aos-duration="2000" />
       <Projects />
-      <div className="empty"></div>
+
+      <img className="starBg" src={starImg} alt="" ref={starBgRef} />
       <img className="bottomImg" src={bottomImg} alt="" />
     </div>
   );
